99bc44f2d60f36e10bcd498098d6b19b570d803a
Committee <nul@mail.org>
2024-11-13 17:20:23 +0200

    A simple GUI

--- src/main/java/com/mycompany/fractals/Fractals.java
+++ src/main/java/com/mycompany/fractals/Fractals.java
@@ -7,6 +7,6 @@ package com.mycompany.fractals;
  */
 public class Fractals {
     public static void main(String[] args) {
-        System.out.println("Test.");
+        System.out.println("Hello.");
     }
 }
