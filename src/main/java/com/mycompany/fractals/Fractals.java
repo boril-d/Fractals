83325e0bdde1c9ea769836f2bd628f7715d426f8
Committee <nul@mail.org>
2024-11-13 09:42:08 +0200

    NetBeans project

---
+++ src/main/java/com/mycompany/fractals/Fractals.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.fractals;
+
+/**
+ *
+ * @author boril.d & bilyana.kr
+ */
+public class Fractals {
+    public static void main(String[] args) {
+        System.out.println("Test.");
+    }
+}
