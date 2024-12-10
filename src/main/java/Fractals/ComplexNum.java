64c6e14b18a152a0eef76e2e946904e5da4e92dd
Boril <none@mail.com>
2024-12-10 19:00:45 +0200

    Formatting and naming fix

--- src/main/java/Fractals/ComplexNum.java
+++ src/main/java/Fractals/ComplexNum.java
@@ -9,62 +9,62 @@ package Fractals;
  * @author bilyana-kr 
  */
 public class ComplexNum {
-     private double x;
+    private double x;
     private double y;
     
-    public double getX(){
+    public double getX() {
         return x;
     }
-    public void setX (double x){
+    public void setX(double x) {
         this.x = x;
     }
     
-    public double getY(){
+    public double getY() {
         return y;
     }
-    public void setY (double y){
+    public void setY(double y) {
         this.y = y;
     }
     
-    public ComplexNum (double x, double y){
+    public ComplexNum(double x, double y) {
         setX(x);
         setY(y);
     }
     
-    public ComplexNum (){
-        this(0,0);
+    public ComplexNum() {
+        this(0, 0);
     }
     
-    public ComplexNum (ComplexNum obj){
+    public ComplexNum(ComplexNum obj) {
         this(obj.getX(), obj.getY());
     }
     
-    public static ComplexNum SumOfCmplxNum(ComplexNum obj1, ComplexNum obj2){
+    public static ComplexNum sum(ComplexNum obj1, ComplexNum obj2) {
         double realPart = obj1.getX() + obj2.getX();
         double imgPart = obj1.getY() + obj2.getY();
-        return new ComplexNum (realPart, imgPart);
+        return new ComplexNum(realPart, imgPart);
     }
     
-    public static ComplexNum ProductOfCmplxNum(ComplexNum obj1, ComplexNum obj2){
+    public static ComplexNum product(ComplexNum obj1, ComplexNum obj2) {
         double realPart = obj1.getX()*obj2.getX() - obj1.getY()*obj2.getY();
         double imgPart = obj1.getX()*obj2.getY() + obj1.getY()*obj2.getX();
-        return new ComplexNum (realPart, imgPart);
+        return new ComplexNum(realPart, imgPart);
     }
     
-    public static ComplexNum ReciprocalOfCmplxNum (ComplexNum obj) throws ArithmeticException{
-        if (obj.getX() == 0 && obj.getY() == 0){
+    public static ComplexNum reciprocal(ComplexNum obj) throws ArithmeticException {
+        if (obj.getX() == 0 && obj.getY() == 0) {
             throw new ArithmeticException("Division by 0!");
         }
         double realPart = obj.x / (obj.x*obj.x + obj.y*obj.y);
         double imgPart = -obj.y / (obj.x*obj.x + obj.y*obj.y);
-        return new ComplexNum (realPart, imgPart);
+        return new ComplexNum(realPart, imgPart);
     }
     
-    public static ComplexNum PowerOfCmplxNum (ComplexNum obj, int power) throws IllegalArgumentException {
-        
-        if (power >= -1 && power <= 1){
-            throw new IllegalArgumentException ("The power of z must be > 1 or < -1.");
+    public static ComplexNum power(ComplexNum obj, int power) throws ArithmeticException {
+        if (obj.getX() == 0 && obj.getY() == 0 && power == 0) {
+            throw new ArithmeticException("0 to the power of 0 is undefined!");
         }
+        
         ComplexNum result = new ComplexNum(1, 0);
         
         boolean isNegative = false;
@@ -73,11 +73,13 @@ public class ComplexNum {
             isNegative = true;
             power = -power;
         }
-        for (int i = 0; i < power; i++){
-            result = ProductOfCmplxNum(obj, result);
+        for (int i = 0; i < power; i++) {
+            result = product(obj, result);
         }
-        if (isNegative == true) return ReciprocalOfCmplxNum(result);
-        else{
+          
+        if (isNegative == true) {
+            return reciprocal(result);
+        } else{
             return result;
         }
     }
